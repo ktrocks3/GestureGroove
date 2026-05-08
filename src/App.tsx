34c7a7519bf8a5c7d3f7fea8734225c14549ae07
Kishan Thakurani <kishanthakurani2003@gmail.com>
2026-05-08 12:28:25 +0300

    chore: Page setup stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,116 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <>
+            <section id="center">
+                <h1>Get started</h1>
+                <div className="flex w-9/10 justify-between items-stretch gap-10 flex-1">
+                    <div className="outline-4 w-full">
 
-  return (
-    <>
-      <section id="center">
-        <div>
-          <h1>Get started</h1>
-          <p className="text-green-500">
-            This is green to test if Tailwind is working, it is!
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+                    </div>
+                    <div className="outline-4 w-full">
 
-      <div className="ticks"></div>
+                    </div>
+                </div>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+            </section>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+            <div className="ticks"></div>
+
+            <section id="next-steps">
+                <div id="docs">
+                    <div className="flex items-center justify-start gap-2">
+                        <svg
+                            className="h-5 w-5 shrink-0 translate-y-px-1"
+                            role="presentation"
+                            aria-hidden="true"
+                        >
+                            <use href="/icons.svg#spotify-icon"/>
+                        </svg>
+
+                        <h2 className="m-0 leading-none">Login</h2>
+                    </div>
+                    <ul>
+                        Gotta add the spotify stuff here
+                    </ul>
+                </div>
+                <div id="social">
+                    <div className="flex items-center justify-start gap-2">
+
+                        <svg className="h-5 w-5 shrink-0 translate-y-px-1" role="presentation" aria-hidden="true">
+                            <use href="/icons.svg#social-icon"></use>
+                        </svg>
+
+                        <h2 className="m-0 leading-none">Connect</h2>
+                    </div>
+                    <ul>
+                        Gotta add the contact stuff here
+                    </ul>
+                </div>
+            </section>
+
+            <div className="ticks"></div>
+            <section id="spacer"></section>
+        </>
+    )
 }
 
 export default App
